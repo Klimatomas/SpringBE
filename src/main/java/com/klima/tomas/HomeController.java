eeb482919589f433e427269afa060df27ccf4185
tomas.klima <mrklimatomas@gmail.com>
2017-07-21 15:37:24 +0200

    Updated new employee api endpoint

--- src/main/java/com/klima/tomas/HomeController.java
+++ src/main/java/com/klima/tomas/HomeController.java
@@ -1,8 +1,8 @@
 package com.klima.tomas;
 
 import com.google.gson.Gson;
-import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +17,23 @@ public class HomeController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/newemployee")
-    public String addEmployee(@RequestBody String request){
+    public String addEmployee(@RequestBody String request) {
 
         Employee employee = new Gson().fromJson(request, Employee.class);
         this.repository.save(employee);
 
         return ("index");
     }
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.DELETE, value = "/api/deleteEmployee")
+    public String deleteEmployee(@RequestBody String request) {
+        Employee employee = new Gson().fromJson(request, Employee.class);
+        this.repository.delete(employee.getId());
+
+        return ("index");
+    }
+
+
 
 }
